eb0545b585aaa855b1f0b60b3ecb86fcbde00b4f
code-knayam <mayankjain1394@gmail.com>
2018-01-21 13:41:16 +0530

    Solved Infinite Monkey Theorem.

---
+++ infiniteMonkeyTheorem/game.py
@@ -0,0 +1,52 @@
+import random
+
+def best_character(beststring, goal, i):
+    if beststring[i] == goal[i]:
+        return True
+    else:
+        return False
+
+
+def generator(beststring, goal):
+    alphabets = "abcdefghijklmnopqrstuvwxyz "
+    res = ""
+    if beststring != "":
+        for i in range(28):
+            if best_character(beststring, goal, i):
+                res = res + beststring[i]
+            else:
+                res = res + alphabets[random.randrange(27)]                
+    else:
+        for i in range(28):        
+            res = res + alphabets[random.randrange(27)]
+
+    return res
+
+def scorer(goal, teststring):
+    score = 0
+    for i in range(28):
+        if goal[i] == teststring[i]:
+            score = score + 1
+    return score/28
+
+def game():
+    goal = "methinks it is like a weasel"
+    beststring = ""
+    bestscore = 0
+    newstring = generator(beststring, 28)
+    score = scorer(goal, newstring)
+    times = 1
+    while score < 1:
+        if score > bestscore:
+            bestscore = score
+            beststring = newstring
+            print("Score = %f String = %s Times = %i" %(score, newstring, times))
+        newstring = generator(beststring, goal)
+        score = scorer(goal, newstring)
+        times = times + 1
+        if score == 1:
+            print()
+            print()
+            print("Score = %f String = %s Times = %i" %(score, newstring, times))
+
+game()
